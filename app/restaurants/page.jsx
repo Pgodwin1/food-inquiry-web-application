3c721813c75a27e87829c12df91410aeec55b9a1
Pgodwin1 <pgodwin287@gmail.com>
2024-02-29 09:21:56 +0100

    updatde

--- app/restaurants/page.jsx
+++ app/restaurants/page.jsx
@@ -3,8 +3,6 @@ import React from 'react'
 
 
 export default async function page() {
-    const restaurants = await getRestaurants();
-    // console.log(restaurants)
   return (
     <div>page</div>
   )
